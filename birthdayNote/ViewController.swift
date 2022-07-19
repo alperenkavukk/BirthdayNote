b0df534760e8adea346b418384b248f71b93e89d
alperenkavukk <kavuk.67.76@gmail.com>
2022-07-20 01:58:43 +0300

    Initial Commit

---
+++ birthdayNote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  birthdayNote
+//
+//  Created by Alperen Kavuk on 20.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
